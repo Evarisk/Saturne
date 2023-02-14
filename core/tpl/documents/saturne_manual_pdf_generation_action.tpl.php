f40f384b3c3d133a3c9cc5797da30f32d1b00778
Théo David <theo.evarisk@gmail.com>
2023-02-14 10:48:57 +0100

    #652 [Documents] add: clickable link on document generation success setEventMessage

--- core/tpl/documents/saturne_manual_pdf_generation_action.tpl.php
+++ core/tpl/documents/saturne_manual_pdf_generation_action.tpl.php
@@ -36,7 +36,13 @@ if ($action == 'pdfGeneration') {
 	if ( ! empty($conf->global->MAIN_ODT_AS_PDF) && $conf->global->$manualPdfGenerationConf > 0) {
 		try {
 			$odfHandler->exportAsAttachedPDF($file);
-			setEventMessages($langs->trans("FileGenerated") . ' - ' . $pdfName, null);
+			
+			$documentUrl = DOL_URL_ROOT . '/document.php';
+			$documentPath = preg_split('/\//', $filename, 2);
+			$modulepart   = $documentPath[0];
+			$filepath     = $documentPath[1];
+
+			setEventMessages($langs->trans("FileGenerated") . ' - ' . '<a href='.  $documentUrl . '?modulepart=' . $modulepart . '&amp;file=' . urlencode($filepath) . '&entity='. $conf->entity . '"' . '>' . $pdfName, null);
 		} catch (Exception $e) {
 			$error = $e->getMessage();
 			setEventMessages($langs->transnoentities('FileCouldNotBeGeneratedInPDF') . '<br>' . $langs->transnoentities('CheckDocumentationToEnablePDFGeneration'), null, 'errors');
