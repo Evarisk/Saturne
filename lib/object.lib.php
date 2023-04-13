46a437c135b838a0ae146d7a6cc8f3a92346c240
Nicolas Domenech <nicolas@eoxia.com>
2023-04-13 19:26:05 +0200

    #219 [Lib] add: generic object lib function prepare head

--- lib/object.lib.php
+++ lib/object.lib.php
@@ -104,3 +104,131 @@ function saturne_fetch_all_object_type(string $className = '', string $sortorder
         return -1;
     }
 }
+
+/**
+ * Prepare array of tabs for Object.
+ *
+ * @param  CommonObject $object            Object.
+ * @param  bool         $showAttendantsTab Show attendants tab.
+ * @param  bool         $showNoteTab       Show note tab.
+ * @param  bool         $showDocumentTab   Show document tab.
+ * @param  bool         $showAgendaTab     Show agenda tab.
+ * @return array                           Array of tabs.
+ * @throws Exception
+ */
+function saturne_object_prepare_head(CommonObject $object, bool $showAttendantsTab = false, bool $showNoteTab = true, bool $showDocumentTab = true, bool $showAgendaTab = true): array
+{
+    // Global variables definitions.
+    global $conf, $db, $moduleName, $moduleNameLowerCase, $langs, $user;
+
+    // Load translation files required by the page.
+    saturne_load_langs();
+
+    // Initialize values.
+    $h          = 0;
+    $head       = [];
+    $objectType = $object->element;
+
+    if ($user->rights->$moduleNameLowerCase->$objectType->read) {
+        $head[$h][0] = dol_buildpath('/' . $moduleNameLowerCase . '/view/' . $objectType . '/' . $objectType . '_card.php', 1) . '?id=' . $object->id;
+        $head[$h][1] = '<i class="fas fa-info-circle pictofixedwidth"></i>' . $langs->trans(ucfirst($objectType));
+        $head[$h][2] = 'card';
+        $h++;
+
+        if ($showAttendantsTab) {
+            // Libraries
+            require_once __DIR__ . '/../class/saturnesignature.class.php';
+
+            // Initialize technical objects
+            $signatory = new SaturneSignature($db);
+
+            $signatoriesArray = $signatory->fetchSignatories($object->id, $objectType);
+            if (is_array($signatoriesArray) && !empty($signatoriesArray)) {
+                $nbAttendants = count($signatoriesArray);
+            } else {
+                $nbAttendants = 0;
+            }
+
+            $head[$h][0] = dol_buildpath('/' . $moduleNameLowerCase . '/view/saturne_attendants.php', 1) . '?id=' . $object->id . '&module_name=' . $moduleName . '&object_type=' . $objectType;
+            $head[$h][1] = '<i class="fas fa-file-signature pictofixedwidth"></i>' . $langs->trans('Attendants');
+            if ($nbAttendants > 0) {
+                $head[$h][1] .= '<span class="badge marginleftonlyshort">' . $nbAttendants . '</span>';
+            }
+            $head[$h][2] = 'attendants';
+            $h++;
+        }
+
+        if ((isset($object->fields['note_public']) || isset($object->fields['note_private'])) && $showNoteTab) {
+            $nbNote = 0;
+            if (!empty($object->note_private)) {
+                $nbNote++;
+            }
+            if (!empty($object->note_public)) {
+                $nbNote++;
+            }
+            $head[$h][0] = dol_buildpath('/saturne/view/saturne_note.php', 1) . '?id=' . $object->id . '&module_name=' . $moduleName . '&object_type=' . $objectType;
+            $head[$h][1] = '<i class="fas fa-comment pictofixedwidth"></i>' . $langs->trans('Notes');
+            if ($nbNote > 0) {
+                $head[$h][1] .= (empty($conf->global->MAIN_OPTIMIZEFORTEXTBROWSER) ? '<span class="badge marginleftonlyshort">' . $nbNote . '</span>' : '');
+            }
+            $head[$h][2] = 'note';
+            $h++;
+        }
+
+        if ($showDocumentTab) {
+            require_once DOL_DOCUMENT_ROOT . '/core/lib/files.lib.php';
+            require_once DOL_DOCUMENT_ROOT . '/core/class/link.class.php';
+            $upload_dir = $conf->$moduleNameLowerCase->dir_output . '/' . $objectType . '/' . dol_sanitizeFileName($object->ref);
+            $nbFiles = count(dol_dir_list($upload_dir, 'files', 0, '', '(\.meta|_preview.*\.png)$'));
+            $nbLinks = Link::count($db, $objectType, $object->id);
+            $head[$h][0] = dol_buildpath('/saturne/view/saturne_document.php', 1) . '?id=' . $object->id . '&module_name=' . $moduleName . '&object_type=' . $objectType;
+            $head[$h][1] = '<i class="fas fa-file-alt pictofixedwidth"></i>' . $langs->trans('Documents');
+            if (($nbFiles + $nbLinks) > 0) {
+                $head[$h][1] .= '<span class="badge marginleftonlyshort">' . ($nbFiles + $nbLinks) . '</span>';
+            }
+            $head[$h][2] = 'document';
+            $h++;
+        }
+
+        if ($showAgendaTab) {
+            $head[$h][0] = dol_buildpath('/saturne/view/saturne_agenda.php', 1) . '?id=' . $object->id . '&module_name=' . $moduleName . '&object_type=' . $objectType;
+            $head[$h][1] = '<i class="fas fa-calendar-alt pictofixedwidth"></i>' . $langs->trans('Events');
+            if (isModEnabled('agenda') && (!empty($user->rights->agenda->myactions->read) || !empty($user->rights->agenda->allactions->read))) {
+                $nbEvent = 0;
+                // Enable caching of object type count actioncomm
+                require_once DOL_DOCUMENT_ROOT . '/core/lib/memory.lib.php';
+                $cacheKey = 'count_events_' . $objectType . '_' . $object->id;
+                $dataRetrieved = dol_getcache($cacheKey);
+                if (!is_null($dataRetrieved)) {
+                    $nbEvent = $dataRetrieved;
+                } else {
+                    $sql = 'SELECT COUNT(id) as nb';
+                    $sql .= ' FROM ' . MAIN_DB_PREFIX . 'actioncomm';
+                    $sql .= ' WHERE fk_element = ' . $object->id;
+                    $sql .= " AND elementtype = '" . $objectType . '@' . $moduleNameLowerCase . "'";
+                    $resql = $db->query($sql);
+                    if ($resql) {
+                        $obj = $db->fetch_object($resql);
+                        $nbEvent = $obj->nb;
+                    } else {
+                        dol_syslog('Failed to count actioncomm ' . $db->lasterror(), LOG_ERR);
+                    }
+                    dol_setcache($cacheKey, $nbEvent, 120); // If setting cache fails, this is not a problem, so we do not test result.
+                }
+                $head[$h][1] .= '/';
+                $head[$h][1] .= $langs->trans('Agenda');
+                if ($nbEvent > 0) {
+                    $head[$h][1] .= '<span class="badge marginleftonlyshort">' . $nbEvent . '</span>';
+                }
+            }
+            $head[$h][2] = 'agenda';
+            $h++;
+        }
+    }
+
+    complete_head_from_modules($conf, $langs, $object, $head, $h, $objectType . '@' . $moduleNameLowerCase);
+
+    complete_head_from_modules($conf, $langs, $object, $head, $h, $objectType . '@' . $moduleNameLowerCase, 'remove');
+
+    return $head;
+}
