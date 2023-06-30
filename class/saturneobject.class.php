94635acb435ef87cdb30ffb09d1dfbbd654bc536
evarisk-micka <mickael.evarisk@gmail.com>
2023-06-30 18:04:51 +0200

    #465 [SaturneObject] add: improve getTriggerDescription

--- class/saturneobject.class.php
+++ class/saturneobject.class.php
@@ -639,12 +639,12 @@ abstract class SaturneObject extends CommonObject
 	}
 
 	/**
-	 * Write generic informations of the description of a trigger
+	 * Write generic information of the description of a trigger
 	 *
 	 * @param  Object $object Object calling the trigger
 	 * @return string         Description to display in actioncomm->note_private
 	 */
-	public function getTriggerDescription(object $object)
+	public function getTriggerDescription(object $object): string
 	{
 		global $conf, $db, $langs, $mysoc;
 
@@ -654,17 +654,19 @@ abstract class SaturneObject extends CommonObject
 		$now  = dol_now();
 
 		$ret  = $langs->trans('Ref') . ' : ' . $object->ref . '</br>';
-		$ret .= (!empty($object->label) ? $langs->trans('Label') . ' : ' . $object->label . '</br>' : '');
-		$ret .= (!empty($object->description) ? $langs->trans('Description') . ' : ' . $object->description . '</br>' : '');
+		$ret .= (isset($object->label) && !empty($object->label) ? $langs->trans('Label') . ' : ' . $object->label . '</br>' : '');
+		$ret .= (isset($object->description) && !empty($object->description) ? $langs->trans('Description') . ' : ' . $object->description . '</br>' : '');
+		$ret .= (isset($object->type) && !empty($object->type) ? $langs->trans('Type') . ' : ' . $object->type . '</br>' : '');
+		$ret .= (isset($object->value) && !empty($object->value) ? $langs->trans('Value') . ' : ' . $object->value . '</br>' : '');
 		$ret .= $langs->trans('DateCreation') . ' : ' . dol_print_date($object->date_creation ?: $now, 'dayhoursec', 'tzuser') . '</br>';
 		$ret .= $langs->trans('DateModification') . ' : ' . dol_print_date($object->tms ?: $now, 'dayhoursec', 'tzuser') . '</br>';
 		if (!empty($object->fk_user_creat)) {
 			$user->fetch($object->fk_user_creat);
-			$ret .= $langs->trans('CreatedByLogin') . ' : ' . $user->getNomUrl(0, 'nolink', -1, 1, 24, 0, 'firstelselast') . '</br>';
+			$ret .= $langs->trans('CreatedByLogin') . ' : ' . $user->firstname . ' ' . $user->lastname . '</br>';
 		}
 		if (!empty($object->fk_user_modif)) {
 			$user->fetch($object->fk_user_modif);
-			$ret .= $langs->trans('ModifiedByLogin') . ' : ' . $user->getNomUrl(0, 'nolink', -1, 1, 24, 0, 'firstelselast') . '</br>';
+			$ret .= $langs->trans('ModifiedByLogin') . ' : ' . $user->firstname . ' ' . $user->lastname . '</br>';
 		}
 		$ret .= $langs->trans('EntityNumber') . ' : ' . $conf->entity . '</br>';
 		$ret .= $langs->trans('EntityName') . ' : ' . $mysoc->name . '</br>';
