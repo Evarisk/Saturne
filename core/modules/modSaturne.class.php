45314e1dde911ebabdff51a7a5d18127477ec76b
Nicolas Domenech <nicolas@eoxia.com>
2023-07-21 03:18:28 +0200

    #544 [Mod] add: modAgenda

--- core/modules/modSaturne.class.php
+++ core/modules/modSaturne.class.php
@@ -145,7 +145,7 @@ class modSaturne extends DolibarrModules
         // A condition to hide module
 		$this->hidden = false;
         // List of module class names as string that must be enabled if this module is enabled. Example: array('always1'=>'modModuleToEnable1','always2'=>'modModuleToEnable2', 'FR1'=>'modModuleToEnableFR'...)
-		$this->depends = [];
+		$this->depends = ['modAgenda'];
         foreach ($modulesList as $moduleName => $moduleNameLowerCase) {
             $this->requiredby[] = 'mod' . $moduleName; // List of module class names as string to disable if this one is disabled. Example: array('modModuleToDisable1', ...)
         }
