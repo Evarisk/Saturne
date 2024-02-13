a927954852890c3b3e04c7d17aebc79b4ba2c94c
Charles Delachapelle <charles.delachapelle@evarisk.com>
2024-02-13 15:35:29 +0100

    #842 [SaturneDashboard] fix: parameters of saturne dashboard

--- class/saturnedashboard.class.php
+++ class/saturnedashboard.class.php
@@ -55,11 +55,11 @@ class SaturneDashboard
     /**
      * Load dashboard info
      *
-     * @param array  $moreParams Parameters for load dashboard info
+     * @param array|null  $moreParams Parameters for load dashboard info
      *
      * @return array
      */
-    public function load_dashboard(array $moreParams = []): array
+    public function load_dashboard(?array $moreParams = []): array
     {
         require_once __DIR__ . '/../../' . $this->module . '/class/' . $this->module . 'dashboard.class.php';
 
@@ -88,12 +88,12 @@ class SaturneDashboard
     /**
      * Show dashboard
      *
-     * @param array      $moreParams    Parameters for load dashboard info
+     * @param array|null      $moreParams    Parameters for load dashboard info
      *
      * @return void
      * @throws Exception
      */
-    public function show_dashboard(array $moreParams = [])
+    public function show_dashboard(?array $moreParams = [])
     {
         global $conf, $form, $langs, $moduleNameLowerCase, $user;
 
