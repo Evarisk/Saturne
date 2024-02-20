324e44ecbe2938229fe56bcb7b9d94f0b9cc3d63
Nicolas Domenech <nicolas@eoxia.com>
2024-02-20 16:51:19 +0100

    #650 [Signature] add: rework for use tpl actions instead

--- public/signature/add_signature.php
+++ public/signature/add_signature.php
@@ -121,79 +121,8 @@ if ($resHook < 0) {
 }
 
 if (empty($resHook)) {
-    // Action to add signature
-    if ($action == 'add_signature') {
-        $data = json_decode(file_get_contents('php://input'), true);
-
-        $signatory->signature      = $data['signature'];
-        $signatory->signature_date = dol_now();
-
-        $result = $signatory->update($user, true);
-        if ($result > 0) {
-            // Creation signature OK
-            $signatory->setSigned($user, false, 'public');
-        } elseif (!empty($signatory->errors)) { // Creation signature KO
-            setEventMessages('', $signatory->errors, 'errors');
-        } else {
-            setEventMessages($signatory->error, [], 'errors');
-        }
-    }
-
-    // Action to build doc
-    if ($action == 'builddoc') {
-        $outputLangs = $langs;
-        $newLang = '';
-
-        if ($conf->global->MAIN_MULTILANGS && empty($newLang) && GETPOST('lang_id', 'aZ09')) {
-            $newLang = GETPOST('lang_id', 'aZ09');
-        }
-        if (!empty($newLang)) {
-            $outputLangs = new Translate('', $conf);
-            $outputLangs->setDefaultLang($newLang);
-        }
-
-        // To be sure vars is defined
-        if (empty($hideDetails)){
-            $hideDetails = 0;
-        }
-        if (empty($hideDesc)) {
-            $hideDesc = 0;
-        }
-        if (empty($hideRef)) {
-            $hideRef = 0;
-        }
-        if (empty($moreParams)) {
-            $moreParams = [];
-        }
-
-        $constforval = strtoupper($moduleName) . '_' . strtoupper($documentType) . '_ADDON_ODT_PATH';
-        $template    = preg_replace('/DOL_DOCUMENT_ROOT/', DOL_DOCUMENT_ROOT, $conf->global->$constforval);
-        $model       = strtolower($documentType) . '_odt:' . $template .'template_' . strtolower($documentType) . '.odt';
-
-        $moreParams['object']     = $object;
-        $moreParams['user']       = $user;
-        $moreParams['specimen']   = 1;
-        $moreParams['zone']       = 'public';
-        $moreParams['objectType'] = $objectType;
-
-        $result = $document->generateDocument($model, $outputLangs, $hideDetails, $hideDesc, $hideRef, $moreParams);
-
-        if ($result > 0) {
-            dol_copy($upload_dir . '/' . strtolower($objectType) . 'document' . '/' . $object->ref . '/public_specimen/' . $document->last_main_doc, DOL_DOCUMENT_ROOT . '/custom/' . $moduleNameLowerCase . '/documents/temp/' . $objectType . '_specimen_' . $trackID . '.odt');
-        } else {
-            setEventMessages($document->error, $document->errors, 'errors');
-        }
-    }
-
-    // Action to remove all temp files
-    if ($action == 'remove_file') {
-        $files = dol_dir_list(DOL_DOCUMENT_ROOT . '/custom/' . $moduleNameLowerCase . '/documents/temp/'); // get all file names
-        foreach ($files as $file) {
-            if (is_file($file['fullname'])) {
-                dol_delete_file($file['fullname']);
-            }
-        }
-    }
+    // Actions add_signature, builddoc, remove_file
+    require_once __DIR__ . '/../../core/tpl/actions/signature_actions.tpl.php';
 }
 
 /*
