3c04f3ba121ce68846b68a0ce66fe571fb6e2094
evarisk-francois <francois.evarisk@gmail.com>
2024-05-30 09:22:59 +0200

    #621 [Signature] use ALTER instead of INSERT

--- sql/update.sql
+++ sql/update.sql
@@ -71,4 +71,6 @@ DROP TABLE `llx_digiriskdolibarr_digiriskdocuments_extrafields`;
 ALTER TABLE `llx_saturne_object_documents` CHANGE json json longtext;
 
 -- 1.5.0
-INSERT INTO `llx_saturne_object_signature` (entity, date_creation, tms, import_key, status, role, firstname, lastname, email, phone, society_name, job, civility_code, json, signature_date, signature_location, signature_comment, element_id, element_type, module_name, signature, stamp, last_email_sent_date, signature_url, transaction_url, object_type, fk_object)
+ALTER TABLE `llx_saturne_object_signature` ADD `json` SMALLINT NULL AFTER `society_name`;
+ALTER TABLE `llx_saturne_object_signature` ADD `civility_code` SMALLINT NULL AFTER `society_name`;
+ALTER TABLE `llx_saturne_object_signature` ADD `job` SMALLINT NULL AFTER `society_name`;
