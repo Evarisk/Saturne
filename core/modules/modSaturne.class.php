ee6bfb7fd183d5583dffcf59b56b907184c76370
evarisk-kilyan <kilyan.evarisk@gmail.com>
2024-10-24 17:28:23 +0200

    #1091 [Code] fix: file can't be downloaded

--- core/modules/modSaturne.class.php
+++ core/modules/modSaturne.class.php
@@ -326,6 +326,8 @@ class modSaturne extends DolibarrModules
 			return -1; // Do not activate module if error 'not allowed' returned when loading module SQL queries (the _load_table run sql with run_sql with the error allowed parameter set to 'default')
 		}
 
+        dolibarr_set_const($this->db, 'MAIN_UMASK', '0664', 'chaine', 0, 0, 'global', 1);
+
 		return $this->_init($sql, $options);
 	}
 
