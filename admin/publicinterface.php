28a01690e458179d15ca3606464d3992fcd7c274
Nicolas Domenech <nicolas@eoxia.com>
2025-11-03 11:26:56 +0100

    #1261 [Admin] add: default user

--- admin/publicinterface.php
+++ admin/publicinterface.php
@@ -36,19 +36,16 @@ $moduleNameLowerCase = dol_strtolower($moduleName);
 
 // Build the paths for the required files
 $moduleLibPath   = dol_buildpath($moduleNameLowerCase . '/lib/' . $moduleNameLowerCase . '.lib.php');
-$moduleClassPath = dol_buildpath($moduleNameLowerCase . '/core/modules/mod' . $moduleName . '.class.php');
 
 // Check if files exist before including them
-if (!(is_file($moduleLibPath) && is_readable($moduleLibPath)) || !(is_file($moduleClassPath) && is_readable($moduleClassPath))) {
-    die('Failed to require ' . $moduleNameLowerCase . ' libraries: Files not found. Paths: ' . $moduleLibPath . ' , ' . $moduleClassPath);
+if (!(is_file($moduleLibPath) && is_readable($moduleLibPath))) {
+    die('Failed to require ' . $moduleNameLowerCase . ' libraries: Files not found. Paths: ' . $moduleLibPath);
 }
 // Load Module Libraries
 require_once $moduleLibPath;
-require_once $moduleClassPath;
 
 // Load Dolibarr libraries
 require_once DOL_DOCUMENT_ROOT . '/core/lib/admin.lib.php';
-require_once DOL_DOCUMENT_ROOT . '/core/class/doleditor.class.php';
 
 // Global variables definitions
 global $conf, $db, $hookmanager, $langs, $moduleName, $moduleNameLowerCase, $user;
@@ -57,35 +54,64 @@ global $conf, $db, $hookmanager, $langs, $moduleName, $moduleNameLowerCase, $use
 saturne_load_langs();
 
 // Get parameters
-$action     = GETPOST('action', 'aZ09');
-$backtopage = GETPOST('backtopage', 'alpha');
-
-// Initialize objects
+$action = GETPOST('action', 'aZ09');
 
 // Initialize view objects
 $form = new Form($db);
 
-$hookmanager->initHooks([$moduleNameLowerCase . 'publicinterfaceadmin', 'globalcard']); // Note that conf->hooks_modules contains array
+$hookmanager->initHooks([$moduleNameLowerCase . 'publicinterfaceadmin']); // Note that conf->hooks_modules contains array
 
 // Permissions
-$permissiontoread = $user->hasRight($moduleNameLowerCase, 'adminpage', 'read');
+$permissionToRead = $user->hasRight($moduleNameLowerCase, 'adminpage', 'read');
 
 // Security check
-saturne_check_access($permissiontoread);
+saturne_check_access($permissionToRead);
 
 /*
  * Actions
  */
 
-if ($action == 'set_session_trainer_responsible') {
-    $responsibleId = GETPOST('session_trainer_responsible_id');
-    if ($responsibleId != getDolGlobalInt('DOLIMEET_SESSION_TRAINER_RESPONSIBLE')) {
-        dolibarr_set_const($db, 'DOLIMEET_SESSION_TRAINER_RESPONSIBLE', $responsibleId, 'integer', 0, '', $conf->entity);
+$object     = null;
+$parameters = [];
+$resHook    = $hookmanager->executeHooks('doActions', $parameters, $object, $action); // Note that $action and $object may have been modified by some hooks
+if ($resHook < 0) {
+    setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
+}
+
+if (empty($resHook)) {
+    if ($action == 'set_default_public_interface_user') {
+        $userId   = GETPOSTINT('default_public_interface_user_id');
+        $constKey = dol_strtoupper($moduleNameLowerCase) . '_DEFAULT_PUBLIC_INTERFACE_USER';
+        if ($userId != getDolGlobalInt($constKey)) {
+            dolibarr_set_const($db, $constKey, $userId, 'integer', 0, '', $conf->entity);
+
+            setEventMessage('SavedConfig');
+            header('Location: ' . $_SERVER['PHP_SELF'] . '?module_name=' . $moduleName);
+            exit;
+        }
     }
 
-    setEventMessage('SavedConfig');
-    header('Location: ' . $_SERVER['PHP_SELF']);
-    exit;
+    if ($action == 'create_default_public_interface_user') {
+        $userTmp = new User($db);
+
+        $userTmp->lastname = $langs->transnoentities('DefaultPublicInterfaceUserLastName');
+        $userTmp->login    = 'default_public_interface_user';
+        $userTmp->entity   = 0;
+        $userTmp->employee = 0;
+        $userTmp->setPassword($user);
+
+        $userId = $userTmp->create($user);
+
+        $constKey = dol_strtoupper($moduleNameLowerCase) . '_DEFAULT_PUBLIC_INTERFACE_USER';
+        dolibarr_set_const($db, $constKey, $userId, 'integer', 0, '', $conf->entity);
+
+        $constKey = dol_strtoupper($moduleNameLowerCase) . '_DEFAULT_PUBLIC_INTERFACE_USER_CREATED';
+        dolibarr_set_const($db, $constKey, $userId, 'integer', 0, '', $conf->entity);
+
+        setEventMessage('SavedConfig');
+        header('Location: ' . $_SERVER['PHP_SELF'] . '?module_name=' . $moduleName);
+        exit;
+    }
 }
 
 /*
@@ -98,7 +124,7 @@ $helpUrl = 'FR:Module_' . $moduleName;
 saturne_header(0,'', $title, $helpUrl);
 
 // Subheader
-$linkBack = '<a href="' . ($backtopage ?: DOL_URL_ROOT . '/admin/modules.php?restore_lastsearch_values=1') . '">' . $langs->trans('BackToModuleList') . '</a>';
+$linkBack = '<a href="' . DOL_URL_ROOT . '/admin/modules.php?restore_lastsearch_values=1' . '">' . $langs->trans('BackToModuleList') . '</a>';
 print load_fiche_titre($title, $linkBack, 'title_setup');
 
 // Configuration header
@@ -106,11 +132,11 @@ $preHead = $moduleNameLowerCase . '_admin_prepare_head';
 $head    = $preHead();
 print dol_get_fiche_head($head, 'publicinterface', $title, -1, $moduleNameLowerCase . '_color@' . $moduleNameLowerCase);
 
-print load_fiche_titre($langs->trans('TrainingSessions'), '', '');
+require __DIR__ . '/../../saturne/core/tpl/admin/object/object_const_view.tpl.php';
 
-print '<form method="POST" action="' . $_SERVER['PHP_SELF'] . '">';
+print '<form method="POST" action="' . $_SERVER['PHP_SELF'] . '?module_name=' . $moduleName . '">';
 print '<input type="hidden" name="token" value="' . newToken() . '">';
-print '<input type="hidden" name="action" value="set_session_trainer_responsible">';
+print '<input type="hidden" name="action" value="set_default_public_interface_user">';
 
 print '<table class="noborder centpercent">';
 print '<tr class="liste_titre">';
@@ -120,27 +146,24 @@ print '<td>' . $langs->trans('Value') . '</td>';
 print '</tr>';
 
 print '<tr class="oddeven"><td>';
-print $langs->trans('SessionTrainerResponsible');
+print $langs->trans('PublicInterfaceUser');
 print '</td><td>';
-print $langs->transnoentities('SessionTrainerResponsibleDesc');
+print $langs->transnoentities('PublicInterfaceUserDescription');
 print '</td>';
 
-print '<td class="minwidth400 maxwidth500">';
-print img_picto($langs->trans('User'), 'user', 'class="pictofixedwidth"') . $form->select_dolusers(getDolGlobalInt('DOLIMEET_SESSION_TRAINER_RESPONSIBLE'), 'session_trainer_responsible_id', 1, null, 0, '', '', '0', 0, 0, '', 0, '', 'minwidth400 maxwidth500');
+print '<td>';
+$constKey = dol_strtoupper($moduleNameLowerCase) . '_DEFAULT_PUBLIC_INTERFACE_USER';
+print img_picto($langs->trans('User'), 'user', 'class="pictofixedwidth"') . $form->select_dolusers(getDolGlobalInt($constKey) > 0 ? getDolGlobalInt($constKey) : '', 'default_public_interface_user_id', 1, null, 0, '', '', '0', 0, 0, '', 0, '', 'minwidth400 maxwidth500');
+print ' <a href="' . DOL_URL_ROOT . '/user/card.php?action=create&backtopage=' . urlencode($_SERVER['PHP_SELF'] . '?module_name=' . $moduleName) . '" target="_blank"><span class="fa fa-plus-circle valignmiddle paddingleft" title="' . $langs->trans('AddUser') . '"></span></a>';
+$constKey = dol_strtoupper($moduleNameLowerCase) . '_DEFAULT_PUBLIC_INTERFACE_USER_CREATED';
+if (getDolGlobalInt($constKey) == 0) {
+    print ' <a href="' . $_SERVER['PHP_SELF'] . '?module_name=' . $moduleName . '&action=create_default_public_interface_user"><span class="fa fa-plus-circle valignmiddle paddingleft" title="' . $langs->trans('AddUser') . '"></span></a>';
+}
 print '</td></tr>';
 
 print '</table>';
 print '<div class="tabsAction"><input type="submit" class="butAction" name="save" value="' . $langs->trans('Save') . '"></div>';
 print '</form>';
-
-$constArray['dolimeet'] = [
-    'AnswerPublicInterfaceUseSignatory' => [
-        'name'        => 'AnswerPublicInterfaceUseSignatory',
-        'description' => 'AnswerPublicInterfaceUseSignatoryDescription',
-        'code'        => 'DIGIQUALI_ANSWER_PUBLIC_INTERFACE_USE_SIGNATORY',
-    ]
-];
-require __DIR__ . '/../../saturne/core/tpl/admin/object/object_const_view.tpl.php';
 
 // Page end
 print dol_get_fiche_end();
